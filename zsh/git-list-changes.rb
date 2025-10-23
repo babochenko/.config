4cf509e4661c098f540be02f964ee9d913ce97ce
Denis Babochenko <denis.babochenko@revolut.com>
2025-10-24 00:33:16 +0100

    git-list-changes: -a - include all commits

---
+++ zsh/git-list-changes.rb
@@ -0,0 +1,109 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'net/http'
+require 'optparse'
+require 'pathname'
+
+def main
+  cfg = {short: false, all: false}
+  OptionParser.new do |opts|
+    opts.on('-s', '--short', 'Short output') { cfg[:short] = true }
+    opts.on('-a', '--all', 'All commits') { cfg[:all] = true }
+  end.parse!
+
+  abort <<~USAGE if ARGV.empty?
+  Usage:
+      git-list-changes <start-commit> [dir]
+  Parameters:
+      start-commit (required) - a commit or range of commits (..) to display the diff for
+      dir          (optional) - if you need diff not for entire repository, but for a subdirectory instead
+  Example:
+      git-list-changes d5bff459f963
+      git-list-changes d5bff459f963..e266dd4e6a55 mymodule
+  USAGE
+
+  from, to = parse_range(ARGV[0])
+  dir = ARGV[1] || ''
+  modules = dir.empty? ? find_modules : [dir]
+
+  modules.each { |mod| process_module(mod, from, to, cfg) }
+end
+
+def main_branch
+  %w[main master].find do |name|
+    system("git rev-parse --verify #{name} > /dev/null 2>&1")
+  end || abort("No 'main' or 'master' branch found")
+end
+
+def parse_range(range)
+  from, to = range.split('..', 2)
+  [from, to || "origin/#{main_branch}"]
+end
+
+def find_modules
+  `find . -maxdepth 2 -name build.gradle`.lines
+    .map { |l| Pathname.new(l.strip).dirname.to_s }
+    .reject { |d| d == '.' }
+end
+
+def process_module(mod, from, to, cfg)
+  short = cfg[:short]
+  commits = cfg[:all] ? '' : "--grep '^Merged'"
+
+  git_log = `git log origin/#{main_branch} --oneline #{from}..#{to} #{commits} -- #{mod}`.strip
+  return if git_log.empty?
+
+  puts "\n>>>> #{mod}" unless mod.empty?
+
+  git_log.lines.each do |commit|
+    hash, msg = commit.strip.split(' ', 2)
+    puts short ? "\e[33m#{hash}\e[0m #{msg}" : "\n\e[33m#{hash}\e[0m #{msg}"
+
+    if match = msg.match(/pull request #(\d+)/)
+      show_pr(match[1], short)
+    end
+  end
+end
+
+def show_pr(pr_num, short)
+  user = ENV['X_BITBUCKET_USER']
+  pass = ENV['X_BITBUCKET_PW']
+  repo = ENV['X_BITBUCKET_REPOSITORY']
+  return unless user && pass && repo
+
+  dir = Pathname.pwd.basename.to_s
+  uri = URI("https://api.bitbucket.org/2.0/repositories/#{repo}/#{dir}/pullrequests/#{pr_num}?fields=title,author.nickname,updated_on")
+
+  begin
+    req = Net::HTTP::Get.new(uri)
+    req.basic_auth(user, pass)
+    req['Accept'] = 'application/json'
+
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true, read_timeout: 10) do |http|
+      http.request(req)
+    end
+
+    return unless res.is_a?(Net::HTTPSuccess)
+
+    data = JSON.parse(res.body)
+    title = data['title']
+    nickname = data['author']['nickname']
+    updated = data['updated_on']
+    ticket = title[/[A-Z]+-[0-9]+/]
+
+    pr_url = "https://bitbucket.org/#{repo}/#{dir}/pull-requests/#{pr_num}"
+
+    if short
+      puts "\t@#{nickname} #{title} (#{pr_url})"
+    else
+      puts "\t@#{nickname} #{pr_url} (#{title})"
+      puts "\tTicket: https://#{repo}.atlassian.net/browse/#{ticket}" if ticket
+      puts "\tMerged At: #{updated}"
+    end
+  rescue StandardError
+    # Silently fail on network/parse errors
+  end
+end
+
+main if __FILE__ == $PROGRAM_NAME
+
