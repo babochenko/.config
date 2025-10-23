33dec71d73915d3e450c18081e8cd580b4c26c25
Denis Babochenko <denisbabochenko@pm.me>
2025-10-23 23:55:40 +0100

    trying to write git-list-changes in ruby

---
+++ git-list-changes.rb
@@ -0,0 +1,90 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'net/http'
+require 'optparse'
+require 'pathname'
+
+def main
+  short = false
+  OptionParser.new do |opts|
+    opts.on('-s', '--short', 'Short output') { short = true }
+  end.parse!
+
+  abort 'Usage: git-list-changes [-s] <commit-range> [dir]' if ARGV.empty?
+
+  from, to = parse_range(ARGV[0])
+  dir = ARGV[1] || ''
+  modules = dir.empty? ? find_modules : [dir]
+
+  modules.each { |mod| process_module(mod, from, to, short) }
+end
+
+def parse_range(range)
+  parts = range.split('..')
+  parts.size == 2 ? parts : [range, 'origin/master']
+end
+
+def find_modules
+  `find . -maxdepth 2 -name build.gradle`.lines
+    .map { |l| Pathname.new(l.strip).dirname.to_s }
+    .reject { |d| d == '.' }
+end
+
+def process_module(mod, from, to, short)
+  git_log = `git log origin/master --oneline #{from}..#{to} --grep "^Merged" -- #{mod}`.strip
+  return if git_log.empty?
+
+  puts "\n>>>> #{mod}" unless mod.empty?
+
+  git_log.lines.each do |commit|
+    hash, msg = commit.strip.split(' ', 2)
+    puts short ? "\e[33m#{hash}\e[0m #{msg}" : "\n\e[33m#{hash}\e[0m #{msg}"
+
+    if match = msg.match(/pull request #(\d+)/)
+      show_pr(match[1], short)
+    end
+  end
+end
+
+def show_pr(pr_num, short)
+  user = ENV['X_BITBUCKET_USER']
+  pass = ENV['X_BITBUCKET_PW']
+  repo = ENV['X_BITBUCKET_REPOSITORY']
+  return unless user && pass && repo
+
+  dir = Pathname.pwd.basename.to_s
+  uri = URI("https://api.bitbucket.org/2.0/repositories/#{repo}/#{dir}/pullrequests/#{pr_num}?fields=title,author.nickname,updated_on")
+
+  begin
+    req = Net::HTTP::Get.new(uri)
+    req.basic_auth(user, pass)
+    req['Accept'] = 'application/json'
+
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true, read_timeout: 10) do |http|
+      http.request(req)
+    end
+
+    return unless res.is_a?(Net::HTTPSuccess)
+
+    data = JSON.parse(res.body)
+    title = data['title']
+    nickname = data['author']['nickname']
+    updated = data['updated_on']
+    ticket = title[/[A-Z]+-[0-9]+/]
+
+    pr_url = "https://bitbucket.org/#{repo}/#{dir}/pull-requests/#{pr_num}"
+
+    if short
+      puts "\t@#{nickname} #{title} (#{pr_url})"
+    else
+      puts "\t@#{nickname} #{pr_url} (#{title})"
+      puts "\tTicket: https://#{repo}.atlassian.net/browse/#{ticket}" if ticket
+      puts "\tMerged At: #{updated}"
+    end
+  rescue StandardError
+    # Silently fail on network/parse errors
+  end
+end
+
+main #if __FILE__ == $PROGRAM_NAME
+
