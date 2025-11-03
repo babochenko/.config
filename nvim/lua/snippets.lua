bc6d17f9c024fad7eb7490804865259a07e6d5fc
Denis Babochenko <denis.babochenko@revolut.com>
2025-11-03 19:49:54 +0000

    more snippets

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -1,4 +1,5 @@
 local snip = require("luasnip")
+local snip_add = require("snippets_add")
 local s = snip.snippet
 local t = snip.text_node
 local f = snip.function_node
@@ -57,44 +58,7 @@ local function sql_snippets()
   local db_name = get_db()
   vim.g.db = db_name
 
-  if db_name == "authentication" then
-    snippets = concat(snippets, {
-      s("devices", t_multiline([[
-        select id, last_used_date, state, brand, app_name, device_token
-        from devices
-        where 1 = 1
-        -- and owner_id = ''
-        -- and brand = 'Apple'
-        -- and brand = 'Browser'
-        -- and state = 'ACTIVE'
-        order by last_used_date desc
-      ]])),
-    })
-
-  elseif db_name == 'market-comms' then
-    snippets = concat(snippets, {
-        s('recipients', t_multiline([[
-            select * from notification_recipients
-            where 1 = 1
-            -- and user_id = ''
-            -- and base_currency = ''
-            order by created_date desc
-        ]])),
-    })
-
-  elseif db_name == 'hermes_central' then
-    snippets = concat(snippets, {
-        s('push_messages', t_multiline([[
-            select dispatch_metadata, * from push_message
-            where 1 = 1
-            and from_service = 'market-comms'
-            -- and user_id = '7c8657f4-6de7-4e64-b42b-446557713609'
-            and created_date > now() - interval '2 hour'
-            order by created_date desc
-        ]])),
-    })
-
-  end
+  snippets = snip_add.additional_sql_snippets(snippets, db_name)
   
   return snippets
 end
