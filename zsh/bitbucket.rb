b0e9cb52e325622025d14dcc9279a4463d40ee49
Denis Babochenko <denis.babochenko@revolut.com>
2026-03-03 11:22:57 +0000

    git-review-reply: group comments by threads

--- zsh/bitbucket.rb
+++ zsh/bitbucket.rb
@@ -65,6 +65,33 @@ def find_open_pr_for_branch(branch)
   end
 end
 
+def group_pr_comments(comments)
+  inline, general = comments.partition { |c| c[:file] }
+
+  by_id = inline.each_with_object({}) { |c, h| h[c[:id]] = c }
+
+  thread_root = lambda do |c|
+    c[:parent_id] && by_id[c[:parent_id]] ? thread_root.call(by_id[c[:parent_id]]) : c
+  end
+
+  grouped = inline.group_by { |c| c[:file] }.transform_values do |file_comments|
+    file_comments
+      .group_by { |c| thread_root.call(c)[:id] }
+      .values
+      .sort_by { |t| thread_root.call(t.first)[:created_at] || '' }
+      .map do |thread|
+        root = thread_root.call(thread.first)
+        {
+          line: root[:line],
+          thread: thread.sort_by { |c| c[:created_at] || '' }
+        }
+      end
+  end
+
+  grouped[:general] = general unless general.empty?
+  grouped
+end
+
 def fetch_pr_comments(pr_num)
   user = ENV['X_BITBUCKET_USER']
   pass = ENV['X_BITBUCKET_PW']
--- zsh/git-review-reply.rb
+++ zsh/git-review-reply.rb
@@ -12,31 +12,7 @@ def main
   comments = fetch_pr_comments(pr[:id])
   abort("Failed to fetch comments") unless comments
 
-  inline, general = comments.partition { |c| c[:file] }
-
-  by_id = inline.each_with_object({}) { |c, h| h[c[:id]] = c }
-
-  thread_root = lambda do |c|
-    c[:parent_id] && by_id[c[:parent_id]] ? thread_root.call(by_id[c[:parent_id]]) : c
-  end
-
-  grouped = inline.group_by { |c| c[:file] }.transform_values do |file_comments|
-    file_comments
-      .group_by { |c| thread_root.call(c)[:id] }
-      .values
-      .sort_by { |t| thread_root.call(t.first)[:created_at] || '' }
-      .map do |thread|
-        root = thread_root.call(thread.first)
-        {
-          line: root[:line],
-          thread: thread.sort_by { |c| c[:created_at] || '' }
-        }
-      end
-  end
-
-  grouped[:general] = general unless general.empty?
-
-  puts JSON.pretty_generate(grouped)
+  puts JSON.pretty_generate(group_pr_comments(comments))
 end
 
 main if __FILE__ == $PROGRAM_NAME
