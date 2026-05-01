9b2edaf384e650c2446365b49ab0a0c11c8c511c
Denis Babochenko <denisbabochenko@pm.me>
2026-05-01 15:54:04 +0100

    nvim: lazy-load telescope in clipboard extension

--- nvim/lua/ext/clipboard.lua
+++ nvim/lua/ext/clipboard.lua
@@ -1,10 +1,3 @@
-local telescope = require('telescope')
-local pickers = require('telescope.pickers')
-local finders = require('telescope.finders')
-local conf = require('telescope.config').values
-local actions = require('telescope.actions')
-local action_state = require('telescope.actions.state')
-
 local function get_clipboard_file()
   local env_path = os.getenv('NVIM_CLIPBOARD_HISTORY')
   if env_path then
@@ -140,6 +133,12 @@ local function copy_to_clipboard(text)
 end
 
 local function show_clipboard_history()
+  local pickers = require('telescope.pickers')
+  local finders = require('telescope.finders')
+  local conf = require('telescope.config').values
+  local actions = require('telescope.actions')
+  local action_state = require('telescope.actions.state')
+
   local history = read_clipboard_history()
 
   if #history == 0 then
