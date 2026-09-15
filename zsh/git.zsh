f139c60a8c21943dd18f41126bf039cb17e820f3
Denis Babochenko <denisbabochenko@pm.me>
2026-09-15 15:18:06 +0100

    zsh: add ten-commit ggc log overlay

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -159,6 +159,8 @@ function gitpp()                  { gitcc $@; git push -u origin HEAD && "$CFGS/
 
 alias gd='git-diff'
 alias gg='git-log'
+function compact-git-log() { GIT_LOG_LINES=10 git-log "$@"; }
+alias ggc='compact-git-log'
 alias gitri='git-rebase-interactive'
 alias gitsm='git-switch-master'
 alias grestore='git-restore'
